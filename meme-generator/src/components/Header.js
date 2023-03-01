a18fbe754ad90d52865afe893fc45aab93a1621d
someid <hannahpietersen@gmail.com>
2023-03-01 18:51:25 +0530

    added code to files

---
+++ meme-generator/src/components/Header.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+export default function Header(){
+    return (
+        
+        <header className="header">
+            <img src="./images/troll-face.png" 
+            alt="troll-face"
+            className="header-img"/>
+            <h2 className="header-title">Meme Generator</h2>
+            <h3 className="header-project">React Project</h3>
+        
+        </header>
+    )
+}  
---
+++ meme-generator/src/components/Meme.js
@@ -0,0 +1,28 @@
+import React from "react"
+
+export default function Meme(){
+    return (
+       <main>
+        <div className="form">
+            <input 
+            type="text" 
+            className="form-input"
+            placeholder="Top Text">
+
+            </input>
+            <input 
+            type="text"
+            className="form-input"
+            placeholder="Bottom Text"
+            >
+
+            </input>
+            <button 
+            
+            className="form-button">
+                Get a new meme image 🖼️
+                </button>
+        </div>
+       </main>
+    )
+}
